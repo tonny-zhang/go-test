20e64aec35a20d54ec7db123f02b505c1a18d3a7
tonny <tonny@tonnydeMacBook-Pro.local>
2015-07-04 21:50:39 +0800

    init

---
+++ src/test/main/main.go
@@ -0,0 +1,6 @@
+package main
+
+import myfunc "test/myfunc"
+func main(){
+	myfunc.Hello("Hello my name is tonny new!")
+}
---
+++ src/test/myfunc/funcs.go
@@ -0,0 +1,7 @@
+package myfunc
+
+import "fmt"
+
+func Hello(msg string){
+	fmt.Println(msg + "\n What is your name?")
+}
